b63025bc3c91f28bd9d494dd91404fad44f1a721
paulocoder-dev <paulo.coder82@gmail.com>
2026-09-01 22:44:10 +0000

    refactor : atualizar tipo e esquema de validação de clientes

--- src/schemas/customer.schema.ts
+++ src/schemas/customer.schema.ts
@@ -7,7 +7,8 @@ export const createCustomerSchema = z.object({
 			1,
 			'Muito curto: esperava-se um texto com pelo menos 1 caractere.'
 		),
-	email: z.email('Endereço de e-mail inválido.')
+	email: z.email('Endereço de e-mail inválido.'),
+	imageUrl: z.url().optional()
 });
 
 export const updateCustomerSchema = z.object({
